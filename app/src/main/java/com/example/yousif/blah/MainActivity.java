56ba45aa854ea6b15fbdee4a0f4b9e932e8a1ff6
Yousif Majid <yousif10majid@gmail.com>
2018-03-04 04:36:32 -0500

    hi

--- app/src/main/java/com/example/yousif/blah/MainActivity.java
+++ app/src/main/java/com/example/yousif/blah/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.yousif.blah;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -16,6 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Name = (EditText)findViewById(R.id.);
+        Name = (EditText) findViewById(R.id.nm);
+        Password = (EditText) findViewById(R.id.pss);
+        Login = (Button) findViewById(R.id.lgn);
+
+        Login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(), Password.getText().toString());
+            });
+        }
     }
+
+    private void validate(String userName, String userPassword) {
+        if ((userName.equals("Hello")) && (userPassword.equals("World"))) {
+            Intent intent = new Intent(MainActivity.this, Page1.class);
+            startActivity(intent);
+        }
+        
 }
