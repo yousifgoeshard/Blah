cfcac5ce12f2d4aba73b1b64440bdfe2bfd8b0b8
Yousif Majid <yousif10majid@gmail.com>
2018-03-04 00:38:57 -0500

    Sup

---
+++ app/src/main/java/com/example/yousif/blah/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.yousif.blah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private Button Login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.);
+    }
+}
---
+++ app/src/main/java/com/example/yousif/blah/Page1.java
@@ -0,0 +1,13 @@
+package com.example.yousif.blah;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Page1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page1);
+    }
+}
